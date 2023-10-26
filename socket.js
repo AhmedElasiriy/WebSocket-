58715d66ff440c3b59dd1f51f8e05229dd23395f
Ahmed Mostafa <ahmed.elasiri1@gmail.com>
2023-10-26 14:40:19 +0300

    Refactor socket.js and index.html for improved messaging functionality

--- socket.js
+++ socket.js
@@ -7,11 +7,19 @@ function initializeSocket(httpServer) {
         }
     })
     oi.on('connection', (socket) => {
-        console.log('a user connected');
+        console.log('new user connected');
 
         socket.on('clientMessage', (data) => {
-            console.log('clientMessage', data);
-        })
+            console.log('clientMessage =>', data);
+            // socket.emit // send message to client that connected to this socket only
+            socket.emit('serverMessage', { message: 'send message to client that connected to this socket only' })
+            // io.emit // send message to all clients that connected to this socket 
+            oi.emit('serverMessage', { message: 'send message to all clients that connected to this socket' })
+            // socket.broadcast.emit // send message to all clients that connected to this socket except the sender client 
+            socket.broadcast.emit('serverMessage', { message: 'send message to all clients except the sender client' })
+        });
+
+
 
     });
 
